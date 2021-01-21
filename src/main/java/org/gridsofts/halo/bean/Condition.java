de80d3f742fe2055b76085ea8282b8a05fbb37de
zholey <lei.zhao@gridsofts.com>
2021-01-21 11:39:54 +0800

    优化构造方法，让编程更便利

--- src/main/java/org/gridsofts/halo/bean/Condition.java
+++ src/main/java/org/gridsofts/halo/bean/Condition.java
@@ -6,6 +6,7 @@
 package org.gridsofts.halo.bean;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -19,25 +20,25 @@ public class Condition implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private List<Param> paramList = new ArrayList<>();
-	
+
 	public static Condition newInstance() {
 		return new Condition();
 	}
-	
+
 	public Condition put(Param param) {
 		paramList.add(param);
 		return this;
 	}
-	
+
 	public Condition put(String paramName, Object paramValue) {
 		paramList.add(new Param(paramName, paramValue));
 		return this;
 	}
-	
+
 	public Iterator<Param> iterator() {
 		return paramList.iterator();
 	}
-	
+
 	/**
 	 * 条件参数；名值对
 	 * 
@@ -45,38 +46,49 @@ public class Condition implements Serializable {
 	 */
 	public static class Param implements Serializable {
 		private static final long serialVersionUID = 1L;
-		
+
 		public static enum Type {
 			String, Number, DateTime
 		}
-		
+
 		public static enum Association {
 			Exact, Fuzzy
 		}
-		
+
 		// 参数类型
 		private Type type = Type.String;
 		// 关联类型
 		private Association association = Association.Exact;
-		
+
 		// 参数名；字段名
 		private String name;
 		// 参数值
 		private Object value;
-		
+
 		public Param(Type type, String name, Object value, Association association) {
 			this.type = type;
 			this.name = name;
 			this.value = value;
 			this.association = association;
 		}
-		
+
 		public Param(Type type, String name, Object value) {
 			this(type, name, value, Association.Exact);
 		}
-		
+
 		public Param(String name, Object value) {
 			this(Type.String, name, value, Association.Exact);
+
+			if (value instanceof Integer || value instanceof Long || value instanceof Float
+					|| value instanceof Double || value instanceof BigDecimal) {
+				this.type = Type.Number;
+			}
+		}
+
+		public Param(String name, Object value, Association association) {
+			this(name, value);
+			
+			this.association = association;
 		}
 
 		public Type getType() {
